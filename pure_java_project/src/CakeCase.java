21139b94abf4c43d31539ac3ffb87a4efd0a1c7b
DihnVu <23017241@st.phenikaa-uni.edu.vn>
2025-05-09 08:12:00 +0700

    Delete pure_java_project/src/CakeCase.java
    Signed-off-by: DihnVu <23017241@st.phenikaa-uni.edu.vn>

--- pure_java_project/src/CakeCase.java
+++
@@ -1,9 +0,0 @@
-public class CakeCase {
-    public static void cake(int type) {
-        if (type == 1) {
-            System.out.println("Bạn chọn bánh kem!");
-        } else {
-            System.out.println("Không có loại bánh này.");
-        }
-    }
-}
