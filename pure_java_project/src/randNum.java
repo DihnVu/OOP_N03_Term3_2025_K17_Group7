9aefacb1b385870b10d90eab4525ef655c0937f7
DihnVu <23017241@st.phenikaa-uni.edu.vn>
2025-05-09 08:12:19 +0700

    Delete pure_java_project/src/randNum.java
    Signed-off-by: DihnVu <23017241@st.phenikaa-uni.edu.vn>

--- pure_java_project/src/randNum.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class randNum {
-    public static int randNum() {
-        Random r = new Random();
-        return r.nextInt(100); // số ngẫu nhiên từ 0 đến 99
-    }
-}
