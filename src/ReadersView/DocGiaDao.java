3281ce060938c394727dcdc265cd77bcba9fcabd
DihnVu <23017241@st.phenikaa-uni.edu.vn>
2025-05-30 09:24:35 +0700

    Update DocGiaDao.java
    Signed-off-by: DihnVu <23017241@st.phenikaa-uni.edu.vn>

--- src/ReadersView/DocGiaDao.java
+++ src/ReadersView/DocGiaDao.java
@@ -34,3 +34,23 @@ public class DocGiaDAO {
         stmt.executeUpdate();
     }
 }
+
+
+btnLuu.addActionListener(new addActionListener(){
+    public void actionPerformed(ActionEvent e){
+        try{
+            String ma = txtMaDocGia.getText();
+            String ten = txtHoTen.getText();
+            String diaChi = txtDiaChi.getText();
+            String sdt = txtSoDienThoai.getText();
+
+            DocGia docGia = new DocGia(ma, ten, diaChi, sdt);
+            DocGiaDao dao = new DocGiaDao();
+            dao.themDocGia(docGia);
+            
+            JOptionPane.showMessageDialog(null, "thêm độc giả thành công!");
+        }  catch( Exception ex) {
+            JOptionPane.showMessageDialog(null, "lỗi: "+ ex.getMessage());
+        }
+    } 
+});
